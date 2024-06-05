a66bec88defdf7ba50967988ffebee552973e191
Vinicius Duarte <viniciusduarte@outlook.com>
2024-06-05 19:22:50 +0100

    init project

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    Poppins: require('../assets/fonts/Poppins-Regular.ttf'),
+    PoppinsLight: require('../assets/fonts/Poppins-Light.ttf'),
+    PoppinsSemiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+    PoppinsExtraBold: require('../assets/fonts/Poppins-ExtraBold.ttf')
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
+}
---
+++ constants/Colors.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+  red: '#d90504',
+  white: '#fafafa',
+  yellow: '#fccd01'
+}
